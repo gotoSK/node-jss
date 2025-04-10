a1cd03d7328eb609ea82da7da12a943eb5d7a1f1
Sugam Karki <karkisugam34@gmail.com>
2025-04-10 20:04:24 +0545

    express

---
+++ web-server-express/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.send('Hello World!');
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.sendFile(301, '/new-page.html');  // 302 by default
+});
+
+// always on bottom of server.js
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
